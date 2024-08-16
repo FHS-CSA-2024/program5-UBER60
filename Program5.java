378841ba05fadd84b97ac2538df6a5fb53d99324
UBER60 <rcho5318@gmail.com>
2024-08-16 11:19:41 +0800

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -1,7 +1,35 @@
 //import stuff here?
 
 //Your code here
-
+public class Program5{
+    public static void main(String[] args){
+      double mile;
+      double gallon;
+      double milePerGallon;
+      
+      System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:" );
+      System.out.println();
+      mile = (double) 286;
+      gallon = (double) 9;
+      milePerGallon = mile/gallon;
+      System.out.println("Royale averaged " + milePerGallon + " m/g");
+      System.out.println();
+      mile = (double) 412;
+      gallon = (double) 40;
+      milePerGallon = mile/gallon;
+      System.out.println("Royale averaged " + milePerGallon + " m/g");
+      System.out.println();
+      mile = (double) 361;
+      gallon = (double) 18;
+      milePerGallon = mile/gallon;
+      System.out.println("Royale averaged " + milePerGallon + " m/g");
+      System.out.println();
+      mile = (double) 161;
+      gallon = (double) 11;
+      milePerGallon = mile/gallon;
+      System.out.println("Royale averaged " + milePerGallon + " m/g");
+    }
+}
 //Paste console output below:
 /*
 
