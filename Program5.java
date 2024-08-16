ccd0d888b27ac245fe6108548e7d89bc116d026d
UBER60 <rcho5318@gmail.com>
2024-08-16 14:04:45 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -12,34 +12,34 @@ public class Program5{
       mile = (double) 286;
       gallon = (double) 9;
       milePerGallon = mile/gallon;
-      System.out.println("Royale averaged " + milePerGallon + " m/g");
+      System.out.println("Royale averaged " + Math.round(milePerGallon*10)/10.0 + " m/g");
       System.out.println();
       mile = (double) 412;
       gallon = (double) 40;
       milePerGallon = mile/gallon;
-      System.out.println("Royale averaged " + milePerGallon + " m/g");
+      System.out.println("Royale averaged " + Math.round(milePerGallon*10)/10.0 + " m/g");
       System.out.println();
       mile = (double) 361;
       gallon = (double) 18;
       milePerGallon = mile/gallon;
-      System.out.println("Royale averaged " + milePerGallon + " m/g");
+      System.out.println("Royale averaged " + Math.round(milePerGallon*10)/10.0 + " m/g");
       System.out.println();
       mile = (double) 161;
       gallon = (double) 11;
       milePerGallon = mile/gallon;
-      System.out.println("Royale averaged " + milePerGallon + " m/g");
+      System.out.println("Royale averaged " + Math.round(milePerGallon*10)/10.0 + " m/g");
     }
 }
 //Paste console output below:
 /*
 Mushroom Cup Prix Racer Average Miles/Per Gallon:
 
-Royale averaged 31.77777777777778 m/g
+Royale averaged 31.8 m/g
 
 Royale averaged 10.3 m/g
 
-Royale averaged 20.055555555555557 m/g
+Royale averaged 20.1 m/g
 
-Royale averaged 14.636363636363637 m/g
+Royale averaged 14.6 m/g
 
 */
